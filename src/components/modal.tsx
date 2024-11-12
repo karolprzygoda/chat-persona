f695ca456d50f6c9eeabce0e756941e3aae7811b
karol <karolprzygodastudia@gmail.com>
2024-11-12 02:56:59 +0100

    Fixed mobile modal

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -23,7 +23,11 @@ const Modal = ({ children }: { children: ReactNode }) => {
 
   return (
     <AlertDialog defaultOpen={true} open={true} onOpenChange={handleOpenChange}>
-      <AlertDialogContent>
+      <AlertDialogContent
+        className={
+          "h-screen min-w-full border-0 md:h-auto md:min-w-0 md:max-w-lg md:border"
+        }
+      >
         <AlertDialogCancel className="absolute right-4 top-4 rounded-sm border-0 bg-transparent opacity-70 shadow-none ring-offset-card transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
           <X className="h-4 w-4" />
           <span className="sr-only">Close</span>
